65121eb6a47fd904cab83e28f971b9590a7114c5
Jennifer Kron <jenniferkron@gmail.com>
2016-06-28 14:25:28 -0400

    Added new pictures for gallery and jquery file

---
+++ jquery.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#hide1").click(function(){
+        $("#spanish1").toggle();
+    });
+
+    $("#hide2").click(function(){
+        $("#spanish2").toggle();
+    });
+
+    $("#hide3").click(function(){
+        $("#spanish3").toggle();
+    });
+
+    $("#hide4").click(function(){
+        $("#design").toggle();
+    });
+
+    $("#hide5").click(function(){
+        $("#other").toggle();
+    });
+
+});
